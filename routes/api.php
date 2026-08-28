d890bfc442f2f9a968f4baba695527d3329868da
kevindexxx <kevinfirmansyah653@gmail.com>
2026-08-28 09:58:52 +0700

    (+) memperbaiki route api.php

--- routes/api.php
+++ routes/api.php
@@ -1,34 +1,21 @@
 <?php
 
-
+use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KomikController;
-use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\PeminjamanController;
-
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/test', function(){
-    return response()->json([
-        'message' => 'API is working'
-    ]);
-});
-
-Route::apiResource('kategori', KategoriController::class);
-Route::apiResource('komiks', KomikController::class);
-Route::apiResource('anggota', AnggotaController::class);
-Route::post('/peminjaman', [PeminjamanController::class, 'store']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::apiResource('kategori', KategoriController::class);
-    Route::apiResource('komiks', KomikController::class);
-    Route::post('/peminjaman', [PeminjamanController::class, 'store']);
+    Route::apiResource('komik', KomikController::class);
+    Route::apiResource('anggota', AnggotaController::class);
+    Route::apiResource('peminjaman', PeminjamanController::class);
+
+    Route::put('/peminjaman/{id}/kembali', [PeminjamanController::class, 'kembali']);
 });
